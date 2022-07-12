60ca926975942b1f6fcc2cb26a2aefef8ba2a4a3
JancugPristel <108045641+JancugPristel@users.noreply.github.com>
2022-07-12 19:35:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6282138729360'],
+  ['6282138729360'],
+  ['6282138729360', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -56,9 +56,9 @@ fs.watchFile(file_exif, () => {
 })
 
 // Sticker WM
-global.packname = sticker_name
-global.author = sticker_author
-global.wm = '© Aine'
+global.packname = drek-md
+global.author = drek_md
+global.wm = '© Drek'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
